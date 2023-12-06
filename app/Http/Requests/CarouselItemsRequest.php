b759857c17a45c9b916ec2a4fdc5395776054682
ianpenalver <jabezpenalver2k20@gmail.com>
2023-12-06 14:29:16 +0800

    Adjustment on Carousel APis

--- app/Http/Requests/CarouselItemsRequest.php
+++ app/Http/Requests/CarouselItemsRequest.php
@@ -23,7 +23,7 @@ class CarouselItemsRequest extends FormRequest
     {
         return [
             'carousel_name'     => 'string|max:255',
-            'image_path'        => 'required|max:255',
+            'image_path'        => 'required|image|mimes:jpg,gif,png,bmp|max:5120',
             'description'       => 'string|nullable|max:255',
             'user_id'           => 'required|integer',
         ];
